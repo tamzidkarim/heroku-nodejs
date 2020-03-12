b303879bc6b49f6974d4bbafef783c8593d203fa
tamzidkarim <e.k.tamzid@gmail.com>
2020-03-12 16:52:16 +0600

    section merged

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const getSubsection = (ws0, from, to) => {
   return value;
 };
 
-const wb = xlsx.readFile('data.xlsx');
+const wb = xlsx.readFile('data.xls');
 
 const sheetNames = wb.SheetNames;
 
@@ -48,7 +48,12 @@ sectionValue[7] = ws0['A122'] ? ws0['A122'].v : undefined;
 // console.log(sectionValue);
 
 //SIBSECTION VALUE
-var subsectionValue = [];
+var subsectionValue = [
+  {
+    merged: '',
+    answer: '',
+  },
+];
 subsectionValue[0] = getSubsection(ws0, 3, 10);
 subsectionValue[1] = getSubsection(ws0, 13, 18);
 subsectionValue[2] = getSubsection(ws0, 21, 55);
@@ -59,20 +64,35 @@ subsectionValue[6] = getSubsection(ws0, 112, 120);
 subsectionValue[7] = getSubsection(ws0, 123, 143);
 
 // console.log(subsectionValue.length);
-
+var mergedFile = [
+  {
+    merged: '',
+    answer: '',
+  },
+];
 for (let i = 0; i < sectionValue.length; i++) {
   file[i] = {
     section: sectionValue[i],
     subsection: subsectionValue[i],
     answer: '',
   };
+  mergedFile[i] = {
+    merged: file[i].section + ' ' + file[i].subsection,
+    answer: '',
+  };
 }
+console.log(mergedFile);
 
 // let jsonFile = JSON.stringify(file);
 // console.log(file);
 
 app.get('/', (req, res) => {
-  res.json(file);
+  res.json(mergedFile);
+});
+
+app.post('/export', (req, res) => {
+  xlsx.writeFile(wb, 'newData.xlsx', { cellStyles: true });
+  res.statusCode(200).send('Excel file generated successsfully');
 });
 
 app.listen(4500);
@@ -87,22 +107,3 @@ app.listen(4500);
 // xlsx.utils.book_append_sheet(newWb, newWs1, 'ws1');
 
 // xlsx.writeFile(wb, 'New Data.xlsx', { cellStyles: true });
-
-var sheet2arr = function(sheet) {
-  var result = [];
-  var row;
-  var rowNum;
-  var colNum;
-  var range = xlsx.utils.decode_range(sheet['!ref']);
-  for (rowNum = range.s.r; rowNum <= range.e.r; rowNum++) {
-    row = [];
-    for (colNum = range.s.c; colNum <= range.e.c; colNum++) {
-      var nextCell = sheet[xlsx.utils.encode_cell({ r: rowNum, c: colNum })];
-      if (typeof nextCell === 'undefined') {
-        row.push(void 0);
-      } else row.push(nextCell.w);
-    }
-    result.push(row);
-  }
-  return result;
-};
